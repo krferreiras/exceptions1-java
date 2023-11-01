be528c3bcbd1f5a6696191d52609f24379046935
Kaio Rodrigo <dossantos.krf@gmail.com>
2023-10-31 21:39:14 -0300

    Bad solution and changing the folder name

--- src/com/example/application/Program.java
+++ src/com/example/application/Program.java
@@ -37,16 +37,12 @@ public class Program {
 				System.out.print("Check-out date (DD/MM/YYYY): ");
 				checkOut  = sdf.parse(sc.next());
 				
-				Date now = new Date();
+				String error = reservation.updateDates(checkIn, checkOut);
 				
-				if(checkIn.before(now) || checkOut.before(now)) {
-					System.out.println("Error in reservation: Reservation dates for "
-							+ "update must be future dates.");
-				}else if (!checkOut.after(checkIn)){
-					System.out.println("Error in reservation: Check-out date must be "
-							+ "after check-in date.");
+				if(error != null) {
+					System.out.println();
+					System.out.println("Error in reservation: " + error);
 				}else {
-					reservation.updateDate(checkIn, checkOut);
 					System.out.println();
 					System.out.println("Reservation: " + reservation);
 				}
